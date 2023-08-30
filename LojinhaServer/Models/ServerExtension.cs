dfd61fdf4a2ba415471bcf752a367773e345d364
Felipe Arruda <flphlk@gmail.com>
2023-08-30 19:49:11 -0300

    crete models folder and files

---
+++ LojinhaServer/Models/ServerExtension.cs
@@ -0,0 +1,13 @@
+ using LojinhaServer.Models;
+ using MongoDB.Driver;
+
+ namespace LojinhaServer.Extensions;
+
+ public static class ServiceExtensions
+ {
+    public static void ConfigureCors(this IServiceCollection services)
+    {
+        
+    }
+
+ }
